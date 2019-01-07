b96d8950f96f15566811f7fc6d71a92faa4fd553
MrMoloc <moloc1234@gmail.com>
2019-01-07 16:48:55 +0100

    I did something
    k m s

--- bw.js
+++ bw.js
@@ -1,11 +1,5 @@
 module.exports = {
     bw : [
-        'kys',
-        'ky$',
-        'kill yourself',
-        'killyourself',
-        'hang urself',
-        'kill urself',
-        'k y s'
+        'Dinoe is very cool'
     ]
 };
